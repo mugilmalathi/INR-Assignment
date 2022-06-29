b8f80428cbb957425228b260acd1fdabcad503f3
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-06-29 13:59:40 +0530

    flow completed

--- inr-assignment/src/App.js
+++ inr-assignment/src/App.js
@@ -1,9 +1,77 @@
+import { useState } from 'react';
 import './App.css';
+import wheel from "./wheel.png"
 
 function App() {
-  return (
+
+  const [ticket, setTicket] = useState("")
+  const [num, setNum] = useState([])
+
+  const ticketcreation = (e)=>{
+   const val = e.target.value;
+   if(ticket.length<=5) setTicket(ticket+val)
+   else alert("Can't add more than 6 numbers..!")
+    console.log(ticket);
+  }
+
+  const handleClick = (e)=>{
+    setNum(...num, ticket)
+  }
+
+  const handleErase=()=>{
+    setTicket("")
+  }
+
+  const randomTicket =()=>{
+    setTicket(Math.floor((Math.random()*1000000)+6))
+    console.log(ticket);
+    
+  }
+
+ return (
     <div className="App">
-     <h1>Helloo</h1>
+
+     <div id='top'>
+      <div id='left'>
+        
+        <div id='left1'>
+          <div>{ticket}</div>
+          <div>Enter 6 Digit</div>
+        </div>
+
+        <div id='left2'>
+          <button value="7" onClick={ticketcreation}>7</button>
+          <button value="8" onClick={ticketcreation}>8</button>
+          <button value="9" onClick={ticketcreation}>9</button>
+          <button value="4" onClick={ticketcreation}>4</button>
+          <button value="5" onClick={ticketcreation}>5</button>
+          <button value="6" onClick={ticketcreation}>6</button>
+          <button value="1" onClick={ticketcreation}>1</button>
+          <button value="2" onClick={ticketcreation}>2</button>
+          <button value="2" onClick={ticketcreation}>3</button>
+          <button onClick={handleErase}>erc</button>
+          <button value="0" onClick={ticketcreation}>0</button>
+          <button>del</button>
+        </div>
+
+        <div id='left3'>
+          <button onClick={handleClick}>ADD TICKET</button>
+        </div>
+
+      </div>
+      <div id='right'>
+        <img onClick={randomTicket} src={wheel} alt="" />
+      </div>
+     </div>
+
+     <div id='down'>
+     <h4>Your Selected Tickets</h4>
+      <div id='ticket'>
+       <div>{ticket}</div>
+       <div>X</div>
+      </div>
+     </div>
+
     </div>
   );
 }
